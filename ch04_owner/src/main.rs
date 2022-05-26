7a9693b867610b3ed0b587654dbf6bfe5d0769f4
ukyoda <black12172001@hotmail.co.jp>
2022-05-26 22:24:50 +0900

    Chapter 4 DONE

--- ch04_owner/src/main.rs
+++ ch04_owner/src/main.rs
@@ -7,6 +7,35 @@ fn takes_ownership_return(s: String) -> String {
     return s;
 }
 
+fn print(s: &String) {
+    println!("print s = {}", s);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+// 文字列を渡すときは&Stringではなく&strを渡したほうが良い
+fn first_word_str(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
 fn main() {
     // ::はString型直下のfrom関数を特定する働きをする演算子 (5章と7章で詳しく議論)
     let s = String::from("hello");
@@ -63,4 +92,37 @@ fn main() {
     let s4 = String::from("hello");
     let s4 = takes_ownership_return(s4);
     println!("s4 = {}", s4);
+
+    // 参照はC++と同じく&を使うっぽいね。
+    // 関数の引数に参照をとることを借用と呼ぶそうな。
+    print(&s4);
+    println!("s4 = {}", s4);
+
+    let mut s5 = s4.clone();
+    let mut s6 = s4.clone();
+    print(&s5);
+    // 参照は通常immutableっぽい。mutableにするならmutをつける
+    let r1 = &mut s5;
+    // 可変な参照は特定のスコープでは1つしか持てない。
+    // → この1つしか持てない制約はimmutableな参照にも適用される。
+    // → 下の例について、s6ではなくs5の参照を作ろうとするとエラーになる。
+    // → immutableな参照だけだったらたぶん複数とることはできそう
+    let r2 = &s6;
+    println!("r1 = {}, r2 = {}", r1, r2);
+
+    // スライス型
+    // Pythonのスライスと同じかな？
+    // Stringのスライスはstr&です。
+    let slice = &s4[0..2];
+    println!("slice = {}", slice);
+    println!("s5[0..4] = {}", &s5[0..4]);
+
+    let s7 = String::from("hello world");
+    let s8 = first_word(&s7);
+    println!("s8 = {}", s8);
+    println!("first_word_str = {}", first_word_str("Hello world"));
+
+    // スライスは配列でも使えるよ。タプルはたぶん無理
+    let arr = [1, 2, 3, 4, 5];
+    let slice_arr = &arr[1..3];
 }
